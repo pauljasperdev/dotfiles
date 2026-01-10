eb4a4c0ef46bc8e7a2bd9a238a4f8c05a6657600
Paul Jasper <mail@pauljasper.dev>
2026-01-10 21:05:51 +0100

    feat(nvim): add vim-tmux-navigator

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -2,9 +2,22 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
-vim.keymap.set("n", "<C-j>", ":cnext<CR>", opts)
-vim.keymap.set("n", "<C-k>", ":cprevious<CR>", opts)
-vim.keymap.set("n", "<leader>R", function()
+
+-- vim.keymap.set("n", "<C-j>", ":cnext<CR>", opts)
+-- vim.keymap.set("n", "<C-k>", ":cprevious<CR>", opts)
+
+
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+-- Run shell command in current file directory visual and normal mode
+vim.keymap.set("v", "<leader>R", function()
 	vim.ui.input({ prompt = "Command: " }, function(command)
 		local dir = vim.fn.expand("%:p:h")
 		if command then -- check for nil in case user cancels
@@ -12,7 +25,7 @@ vim.keymap.set("n", "<leader>R", function()
 		end
 	end)
 end, opts)
-vim.keymap.set("v", "<leader>R", function()
+vim.keymap.set("n", "<leader>R", function()
 	vim.ui.input({ prompt = "Command: " }, function(command)
 		local dir = vim.fn.expand("%:p:h")
 		if command then -- check for nil in case user cancels
