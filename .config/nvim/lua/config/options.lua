63cd3b38a55cccdd1ea2fa23b45458a1aff9e971
Paul Jasper <mail@pauljasper.dev>
2026-01-10 21:48:55 +0100

    feat(nvim): add yank highlight

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -17,3 +17,26 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 		vim.opt.relativenumber = true
 	end,
 })
+
+-- Highlight on yank (Catppuccin Mocha yellow)
+local function set_yank_highlight()
+	-- mocha.yellow = #f9e2af, mocha.base = #1e1e2e
+	vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#f9e2af", fg = "#1e1e2e" })
+end
+
+set_yank_highlight()
+
+local yank_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = yank_group,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 600 })
+	end,
+})
+
+-- Re-apply after :colorscheme (Catppuccin sets highlights on load)
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = yank_group,
+	callback = set_yank_highlight,
+})
