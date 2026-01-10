dd7812da746c11af25b8acc9b2768812f7de65cf
Paul Jasper <mail@pauljasper.dev>
2026-01-10 21:48:34 +0100

    fix(nvim): use rewrite of treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,27 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				highlight = {
-					enable = true,
-				},
-				ensure_installed = {
-					"lua",
-					"typescript",
-					"tsx",
-					"go",
-				},
+			require("nvim-treesitter").setup()
+
+			local group = vim.api.nvim_create_augroup("TreesitterHighlight", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				pattern = "*",
+				callback = function(args)
+					pcall(vim.treesitter.start, args.buf)
+				end,
 			})
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		opts = {
 			max_lines = 1,
 		},
 	},
 }
+
