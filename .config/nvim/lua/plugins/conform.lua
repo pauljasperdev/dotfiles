6618d8e0cc678f3028c30a77f67056aaa6a612dd
Paul Jasper <mail@pauljasper.dev>
2026-01-13 09:37:51 +0100

    chore(nvim): ruff python formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -12,14 +12,15 @@ return {
 			yaml = { "prettierd" },
 			markdown = { "prettierd" },
 			html = { "prettierd" },
-			python = { "isort", "black" },
+			python = { "ruff_format" },
 		},
 		format_on_save = function(bufnr)
 			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 				return
 			end
 			return {
-				timeout_ms = 500,
+				-- ruff_format is fast, but give it breathing room
+				timeout_ms = 2000,
 				lsp_format = "never",
 			}
 		end,
