04b5a789845c03b457fa1c713f4e4518eed081b9
Paul Jasper <mail@pauljasper.dev>
2026-01-14 18:19:15 +0100

    chore: add diffview keymaps

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -14,8 +14,11 @@ return {
 		})
 	end,
 	keys = {
-		{ "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
+		{ "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Working tree diff" },
+		{ "<leader>dm", "<cmd>DiffviewOpen origin/main...HEAD<cr>", desc = "Diff vs main" },
+		{ "<leader>dd", "<cmd>DiffviewOpen HEAD~1..HEAD<cr>", desc = "Last commit diff" },
 		{ "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
 		{ "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "File history" },
 	},
 }
+
