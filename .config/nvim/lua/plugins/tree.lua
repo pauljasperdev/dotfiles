adcb1864e3c75e63effec549c255cd8c60d5265a
Paul Jasper <mail@pauljasper.dev>
2026-01-14 18:18:34 +0100

    chore: show show dotfiles in git tree

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -1,24 +1,24 @@
 return {
-  "kyazdani42/nvim-tree.lua",
-  dependencies = {
-    "kyazdani42/nvim-web-devicons",
-  },
-  lazy = false,
-  keys = {
-    { "<leader>ff", "<cmd>NvimTreeFindFile<cr>", desc = "Find file in filetree" },
-    { "<C-n>",      "<cmd>NvimTreeToggle<cr>",   desc = "Find file in filetree" },
-  },
-  opts = {
-    filters = {
-      custom = { ".git", "node_modules", ".vscode" },
-      dotfiles = true,
-    },
-    git = {},
-    view = {
-      adaptive_size = true,
-      float = {
-        enable = true,
-      },
-    },
-  },
+	"kyazdani42/nvim-tree.lua",
+	dependencies = {
+		"kyazdani42/nvim-web-devicons",
+	},
+	lazy = false,
+	keys = {
+		{ "<leader>ff", "<cmd>NvimTreeFindFile<cr>", desc = "Find file in filetree" },
+		{ "<C-n>", "<cmd>NvimTreeToggle<cr>", desc = "Find file in filetree" },
+	},
+	opts = {
+		filters = {
+			custom = { "node_modules", ".vscode" },
+			dotfiles = false,
+		},
+		git = {},
+		view = {
+			adaptive_size = true,
+			float = {
+				enable = true,
+			},
+		},
+	},
 }
