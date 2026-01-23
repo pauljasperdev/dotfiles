0b2909cf4185ee5b7cb8105ceb60470f3b2746f0
Paul Jasper <mail@pauljasper.dev>
2026-01-23 14:22:34 +0100

    chore(nvim): change keybind for hover

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -51,7 +51,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end, opts) -- jump to next diagnostic in buffer
 
 		opts.desc = "Show documentation for what is under cursor"
-		keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+		keymap.set("n", "gh", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
 		opts.desc = "Restart LSP"
 		keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
