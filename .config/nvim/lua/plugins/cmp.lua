aaae3952cb667adc19c566f2d41f8465f55f74b4
Paul Jasper <mail@pauljasper.dev>
2026-02-12 21:56:20 +0100

    fix(nvim): reserve tab for copilot accept

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -24,15 +24,6 @@ return {
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,16 @@
 return {
-	"github/copilot.vim",
+	{
+		"github/copilot.vim",
+		init = function()
+			vim.g.copilot_no_tab_map = true
+		end,
+		config = function()
+			vim.keymap.set("i", "<Tab>", 'copilot#Accept("\\<Tab>")', {
+				expr = true,
+				replace_keycodes = false,
+				silent = true,
+				desc = "Accept Copilot suggestion",
+			})
+		end,
+	},
 }
