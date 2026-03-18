7496b7a1ad5719b4a8abf54b204cd42ba3dda787
Paul Jasper <mail@pauljasper.dev>
2026-03-18 13:02:02 +0100

    feat(nvim): async format-after-save, biome for JS/TS, FormatEnable/Disable commands

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,27 +1,41 @@
 return {
 	"stevearc/conform.nvim",
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			javascript = { "prettierd" },
-			javascriptreact = { "prettierd" },
-			typescript = { "prettierd" },
-			typescriptreact = { "prettierd" },
-			json = { "prettierd" },
-			jsonc = { "prettierd" },
-			yaml = { "prettierd" },
-			markdown = { "prettierd" },
-			html = { "prettierd" },
+			javascript = { "biome", "prettierd", stop_after_first = true },
+			javascriptreact = { "biome", "prettierd", stop_after_first = true },
+			typescript = { "biome", "prettierd", stop_after_first = true },
+			typescriptreact = { "biome", "prettierd", stop_after_first = true },
 			python = { "ruff_format" },
 		},
-		format_on_save = function(bufnr)
+		format_after_save = function(bufnr)
 			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 				return
 			end
 			return {
-				-- ruff_format is fast, but give it breathing room
-				timeout_ms = 2000,
-				lsp_format = "never",
+				lsp_format = "fallback",
 			}
 		end,
 	},
