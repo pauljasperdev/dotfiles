65fd1b4b12318361d2a683cf25bd41598b70e5e5
Paul Jasper <mail@pauljasper.dev>
2026-03-18 14:32:47 +0100

    fix(nvim): resolve local node_modules oxc_language_server binary

--- .config/nvim/lua/plugins/oxlint.lua
+++ .config/nvim/lua/plugins/oxlint.lua
@@ -3,7 +3,15 @@ return {
 		"neovim/nvim-lspconfig",
 		init = function()
 			vim.lsp.config("oxc_language_server", {
-				cmd = { "oxc_language_server" },
+				cmd = function(dispatchers, config)
+					local cmd = "oxc_language_server"
+					local local_cmd = (config or {}).root_dir
+						and config.root_dir .. "/node_modules/.bin/oxc_language_server"
+					if local_cmd and vim.fn.executable(local_cmd) == 1 then
+						cmd = local_cmd
+					end
+					return vim.lsp.rpc.start({ cmd }, dispatchers)
+				end,
 				filetypes = {
 					"javascript",
 					"javascriptreact",
